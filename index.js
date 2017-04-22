14808df4987cd0f9abbf45457ed927cc54547d63
Denis Olek <denis.olek@gmail.com>
2017-04-23 00:27:54 +0200

    A*

---
+++ index.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const PORT = 3000;
+var logger = require('morgan');
+var waypoints = require('./waypoints');
+var PF = require('pathfinding')
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+  next();
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var matrix = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+app.post('/path', function (req, res) {
+  var grid = new PF.Grid(matrix);
+  var finder = new PF.AStarFinder({
+    allowDiagonal: true,
+    dontCrossCorners: true
+  });
+  var gridBackup = grid.clone();
+  var path = finder.findPath(1, 8, 12, 1, grid);
+
+  var waypointPath = [];
+
+  path.forEach(elem => {
+    waypointPath.push({
+      x: elem[0]*100+100,
+      y: elem[1]*100+150
+    })
+  });
+
+  waypointPath.push({
+    wait: true,
+    fruit: 'banana'
+  });
+
+  path.forEach(elem => {
+    waypointPath.push({
+      x: elem[0]*100+100,
+      y: elem[1]*100+150
+    })
+  });
+
+  res.send(waypointPath);
+})
+
+app.listen(PORT, function () {
+  console.log('App listening on port ', PORT);
+})
