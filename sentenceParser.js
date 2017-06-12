6311314b41fa913ad2dc5485d0952b4fc79ac2af
denisolek <denis.olek@gmail.com>
2017-06-12 14:09:10 +0200

    added support for singular itemtype

--- sentenceParser.js
+++ sentenceParser.js
@@ -22,10 +22,16 @@ function getItemFromSentence(sentence) {
   var nlpLexicon = nlp(sentence, lexicon)
   var nlpLexiconNouns = nlp(nlp(sentence).nouns().toSingular().out('text'), lexicon)
 
+  var itemType = nlpLexicon.match('#Type').normalize().toLowerCase().out();
+
+  if (!itemType) {
+    itemType = nlpLexiconNouns.match('#Type').normalize().toLowerCase().out()
+  }
+
   return {
     color: nlpLexicon.match('#Color').normalize().toLowerCase().out(),
     size: nlpLexicon.match('#Size').normalize().toLowerCase().out(),
-    type: nlpLexiconNouns.match('#Type').normalize().toLowerCase().out(),
+    type: itemType,
     hardness: nlpLexicon.match('#Hardness').normalize().toLowerCase().out()
   };
 }
